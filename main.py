1756dd6749cac45d8ef7d568c3c972d7ddfcb126
munish30monga <munish30monga@gmail.com>
2023-11-30 16:44:54 +0530

    print model summary and complexity

--- main.py
+++ main.py
@@ -17,7 +17,9 @@ from utils import MultiCropWrapper                                  # for deep l
 from collections import OrderedDict                                 # for deep learning functionality   
 from torchsummary import summary                                    # for model summary    
 import argparse                                                     # for command line argument parsing     
-import wandb                                                        # for logging training runs                      
+import wandb                                                        # for logging training runs   
+from icecream import ic                                             # for debugging  
+ic.disable()                                                        # disable icecream debugging                 
 
 def load_pretrained_dino_model(base_model, pretrained_model_path, device):
     # Check if the base model is OSNet
@@ -34,7 +36,8 @@ def load_pretrained_dino_model(base_model, pretrained_model_path, device):
         student = torchvision_models.__dict__[base_model]()
         embed_dim = student.fc.weight.shape[1]
         student.fc = nn.Identity()  # Replace the fully connected layer with Identity
-
+    ic(student)
+    
     # Wrap the student model with MultiCropWrapper and DINOHead
     student = MultiCropWrapper(student, DINOHead(
         embed_dim,
@@ -42,7 +45,7 @@ def load_pretrained_dino_model(base_model, pretrained_model_path, device):
         use_bn=False,
         norm_last_layer=True,
     ))
-    student = student.to(device)
+    ic(student)
 
     # Load the pretrained model weights
     saved_state_dict = torch.load(pretrained_model_path, map_location=device)
@@ -51,7 +54,7 @@ def load_pretrained_dino_model(base_model, pretrained_model_path, device):
         name = k[7:]  # Remove "module." prefix if present
         new_state_dict[name] = v
     student.load_state_dict(new_state_dict)
-
+    
     return student
 
 def extract_vanilla_model(pretrained_model, model_type, datamanager, unfreeze_last_n):
@@ -75,7 +78,7 @@ def extract_vanilla_model(pretrained_model, model_type, datamanager, unfreeze_la
             state_dict_model[name] = state_dict_pretrained[name]
 
     model.load_state_dict(state_dict_model)
-
+    
     # Handle unfreezing of layers
     if unfreeze_last_n == -1:
         for param in model.parameters():        # Make all layers trainable
@@ -93,7 +96,7 @@ def extract_vanilla_model(pretrained_model, model_type, datamanager, unfreeze_la
             if i >= num_children - unfreeze_last_n:
                 for param in child.parameters():
                     param.requires_grad = True
-
+   
     return model
 
 def create_base_models(base_model, datamanager, device):
@@ -106,8 +109,7 @@ def create_base_models(base_model, datamanager, device):
         )
     else:
         model = torchvision_models.__dict__[base_model]()
-    print(model)
-    # quit()
+
     return model.to(device)
     
 def setup_datamanager(dataset_dir, args):
@@ -147,18 +149,18 @@ def main():
     parser.add_argument('--batch_size', type=int, default=64, help='Batch size for training')
     parser.add_argument('--epochs', type=int, default=100, help='Number of epochs to train')
     parser.add_argument('--learning_rate', type=float, default=0.0003, help='Learning rate')
-    parser.add_argument('--pretrained_model_path', type=str, default='./pretrained_models/osnet.pth', help='Path to pretrained DINO model')
+    parser.add_argument('--pretrained_model_path', type=str, default='./pretrained_models/osnet_25k.pth', help='Path to pretrained DINO model')
     parser.add_argument('--pretrained', action='store_true', help='Use pretrained model')
     parser.add_argument('--unfreeze_last_n', type=int, default=-1, help='Number of last layers to unfreeze')
     parser.add_argument('--batch_size_train', type=int, default=32, help='Training batch size for ImageDataManager')
     parser.add_argument('--batch_size_test', type=int, default=100, help='Testing batch size for ImageDataManager')
     parser.add_argument('--random_seed', type=int, default=42, help='Random seed for reproducibility')
-    parser.add_argument('--arch', type=str, default='resnet50', help='Architecture for DINO model')
+    parser.add_argument('--arch', type=str, default='osnet', help='Architecture for DINO model')
     parser.add_argument('--optimizer', type=str, default='adam', help='Optimizer for training')
     parser.add_argument('--lr_scheduler', type=str, default='single_step', help='Learning rate scheduler for training')
     parser.add_argument('--stepsize', type=int, default=20, help='Step size for learning rate scheduler')
     parser.add_argument('--use_wandb', action='store_true', help='Use wandb for logging.')
-    parser.add_argument('--wandb_name', type=str, default='finetune_resnet50', help='Name of wandb project')
+    parser.add_argument('--wandb_name', type=str, default='finetune model', help='Name of wandb project')
     args = parser.parse_args()
     
     # Logging
@@ -204,6 +206,14 @@ def main():
         model = create_base_models(args.arch, datamanager, device)
         print(f"Training {args.arch} model from scratch.")
 
+    # Print model summary
+    print("Model Summary:")
+    summary(model, (3, 256, 128))
+    
+    # Model Complexity
+    print("Model Complexity:")
+    torchreid.utils.compute_model_complexity(model, (1, 3, 256, 128), verbose=True, only_conv_linear=False)
+    
     # Optimizer, Scheduler, Engine
     optimizer = torchreid.optim.build_optimizer(model, optim=args.optimizer, lr=args.learning_rate)
     scheduler = torchreid.optim.build_lr_scheduler(optimizer, lr_scheduler=args.lr_scheduler, stepsize=args.stepsize)
